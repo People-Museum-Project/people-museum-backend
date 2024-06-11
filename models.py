7071731533c66ff4d718d1f420195a489bc8c80f
alas129 <alas129@163.com>
2024-06-11 10:28:05 -0700

    Added model.py to map the table in GCP db instance

---
+++ models.py
@@ -0,0 +1,34 @@
+# models.py
+
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+class Person(Base):
+    __tablename__ = 'person'
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String(255), nullable=False)
+    image = Column(String(255))
+    description = Column(Text)
+    bio_docs = Column(ARRAY(Text))
+    authored_docs = Column(ARRAY(Text))
+    collections = Column(ARRAY(Integer))
+    owner = Column(Integer)
+    share_list = Column(ARRAY(Integer))
+    is_public = Column(Boolean, default=False)
+
+class PeopleCollection(Base):
+    __tablename__ = 'people_collection'
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String(255), nullable=False)
+    image = Column(String(255))
+    description = Column(Text)
+    bio_docs = Column(ARRAY(Text))
+    people = Column(ARRAY(Integer))
+    owner = Column(Integer)
+    share_list = Column(ARRAY(Integer))
+    is_public = Column(Boolean, default=False)
+
+# Define other tables similarly
