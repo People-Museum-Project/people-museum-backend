b36cd1d5befe24359442220449954c32bced9e67
YJ-Junsheng-Ye <overdosedizzy@gmail.com>
2024-06-22 01:00:55 -0700

    datastore_client completed, tests passed.

---
+++ datastore_client.py
@@ -0,0 +1,16 @@
+from google.cloud import datastore
+import os
+
+
+class Client:
+    #  "/Users/single/workspaces/people-museum-backend/peoplemuseumyeah-b85c3138781f.json"
+    #  peoplemuseumyeah
+    def __init__(self):
+        # Set the path to service account key file
+        self.__project_name = os.environ["PROJECT"]
+        self.__credentials = os.environ['GOOGLE_APPLICATION_CREDENTIALS']
+
+    def connect(self):
+        # Initialize the Datastore client
+        client = datastore.Client(project=self.__project_name)
+        return client
