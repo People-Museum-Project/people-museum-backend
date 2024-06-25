9f496173ca0b49f363f193b9dd9ee16e70ae589c
alas129 <alas129@163.com>
2024-06-25 13:26:29 -0700

    Updated test script

--- server_test.sh
+++ server_test.sh
@@ -4,11 +4,11 @@
 BASE_URL="http://127.0.0.1:8080"
 
 # 1. Test index endpoint
-curl -X GET "${BASE_URL}/" -H "Content-Type: application/json"
+curl -X GET "http://127.0.0.1:8080/" -H "Content-Type: application/json"
 echo -e "\n"
 
 # 2. Test addUser endpoint
-curl -X POST "${BASE_URL}/addUser" -H "Content-Type: application/json" -d '{
+curl -X POST "http://127.0.0.1:8080/addUser" -H "Content-Type: application/json" -d '{
   "name": "John Doe",
   "imageLink": "http://example.com/image.jpg",
   "description": "A new user"
@@ -16,13 +16,13 @@ curl -X POST "${BASE_URL}/addUser" -H "Content-Type: application/json" -d '{
 echo -e "\n"
 
 # 3. Test getUser endpoint
-curl -X POST "${BASE_URL}/getUser" -H "Content-Type: application/json" -d '{
+curl -X POST "http://127.0.0.1:8080/getUser" -H "Content-Type: application/json" -d '{
   "userId": "5653635052601344"
 }'
 echo -e "\n"
 
 # 4. Test updateUser endpoint
-curl -X PUT "${BASE_URL}/updateUser" -H "Content-Type: application/json" -d '{
+curl -X PUT "http://127.0.0.1:8080/updateUser" -H "Content-Type: application/json" -d '{
   "userId": "5653635052601344",
   "name": "John Smith",
   "imageLink": "http://example.com/newimage.jpg",
@@ -31,13 +31,13 @@ curl -X PUT "${BASE_URL}/updateUser" -H "Content-Type: application/json" -d '{
 echo -e "\n"
 
 # 5. Test deleteUser endpoint
-curl -X DELETE "${BASE_URL}/deleteUser" -H "Content-Type: application/json" -d '{
+curl -X DELETE "http://127.0.0.1:8080/deleteUser" -H "Content-Type: application/json" -d '{
   "userId": "5653635052601344"
 }'
 echo -e "\n"
 
 # 6. Test addPerson endpoint
-curl -X POST "${BASE_URL}/addPerson" -H "Content-Type: application/json" -d '{
+curl -X POST "http://127.0.0.1:8080/addPerson" -H "Content-Type: application/json" -d '{
   "name": "Jane Doe",
   "imageLink": "http://example.com/person.jpg",
   "description": "A new person",
@@ -47,19 +47,19 @@ curl -X POST "${BASE_URL}/addPerson" -H "Content-Type: application/json" -d '{
 echo -e "\n"
 
 # 7. Test getPersonList endpoint
-curl -X POST "${BASE_URL}/getPersonList" -H "Content-Type: application/json" -d '{
+curl -X POST "http://127.0.0.1:8080/getPersonList" -H "Content-Type: application/json" -d '{
   "userId": "1"
 }'
 echo -e "\n"
 
 # 8. Test getPerson endpoint
-curl -X POST "${BASE_URL}/getPerson" -H "Content-Type: application/json" -d '{
+curl -X POST "http://127.0.0.1:8080/getPerson" -H "Content-Type: application/json" -d '{
   "personId": "5637476211228672"
 }'
 echo -e "\n"
 
 # 9. Test updatePerson endpoint
-curl -X PUT "${BASE_URL}/updatePerson" -H "Content-Type: application/json" -d '{
+curl -X PUT "http://127.0.0.1:8080/updatePerson" -H "Content-Type: application/json" -d '{
   "personId": "5637476211228672",
   "newName": "Jane Smith",
   "newImageLink": "http://example.com/newperson.jpg",
@@ -70,13 +70,13 @@ curl -X PUT "${BASE_URL}/updatePerson" -H "Content-Type: application/json" -d '{
 echo -e "\n"
 
 # 10. Test deletePerson endpoint
-curl -X DELETE "${BASE_URL}/deletePerson" -H "Content-Type: application/json" -d '{
+curl -X DELETE "http://127.0.0.1:8080/deletePerson" -H "Content-Type: application/json" -d '{
   "personId": "5637476211228672"
 }'
 echo -e "\n"
 
 # 11. Test addCollection endpoint
-curl -X POST "${BASE_URL}/addCollection" -H "Content-Type: application/json" -d '{
+curl -X POST "http://127.0.0.1:8080/addCollection" -H "Content-Type: application/json" -d '{
   "userId": "1",
   "name": "Collection 1",
   "imageLink": "http://example.com/collection.jpg",
@@ -86,19 +86,19 @@ curl -X POST "${BASE_URL}/addCollection" -H "Content-Type: application/json" -d 
 echo -e "\n"
 
 # 12. Test getCollectionList endpoint
-curl -X POST "${BASE_URL}/getCollectionList" -H "Content-Type: application/json" -d '{
+curl -X POST "http://127.0.0.1:8080/getCollectionList" -H "Content-Type: application/json" -d '{
   "userId": "1"
 }'
 echo -e "\n"
 
 # 13. Test getCollection endpoint
-curl -X POST "${BASE_URL}/getCollection" -H "Content-Type: application/json" -d '{
+curl -X POST "http://127.0.0.1:8080/getCollection" -H "Content-Type: application/json" -d '{
   "collectionId": "5629654941564928"
 }'
 echo -e "\n"
 
 # 14. Test updateCollection endpoint
-curl -X PUT "${BASE_URL}/updateCollection" -H "Content-Type: application/json" -d '{
+curl -X PUT "http://127.0.0.1:8080/updateCollection" -H "Content-Type: application/json" -d '{
   "collectionId": "5629654941564928",
   "newName": "Updated Collection",
   "newImageLink": "http://example.com/updatedcollection.jpg",
@@ -108,7 +108,7 @@ curl -X PUT "${BASE_URL}/updateCollection" -H "Content-Type: application/json" -
 echo -e "\n"
 
 # 15. Test deleteCollection endpoint
-curl -X DELETE "${BASE_URL}/deleteCollection" -H "Content-Type: application/json" -d '{
+curl -X DELETE "http://127.0.0.1:8080/deleteCollection" -H "Content-Type: application/json" -d '{
   "collectionId": "5629654941564928"
 }'
 echo -e "\n"
