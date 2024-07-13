b62c4747f0fceccac28d21b6367352bd37dd3175
YJ-Junsheng-Ye <overdosedizzy@gmail.com>
2024-07-13 16:02:56 -0700

    origin cor added.

--- run.py
+++ run.py
@@ -1,6 +1,7 @@
 from app import create_app
-
+from flask_cors import CORS
 
 if __name__ == "__main__":
     app = create_app()
-    app.run(host="127.0.0.1", port=8080, debug=True)
+    CORS(app)
+    app.run(host="0.0.0.0", port=8080, debug=True)
