db47912c03f5f24719168ed50d51d2522fdddcf4
YJ-Junsheng-Ye <overdosedizzy@gmail.com>
2024-07-14 13:33:10 -0700

    temp commit for GenAI lib.

--- app/ai_handler.py
+++ app/ai_handler.py
@@ -1,3 +1,14 @@
 # from import
 # different api handler in the class
 
+
+
+
+
+def get_assistant(person_id):
+    # retrieve the same assistant of this person from datastore
+    pass
+
+def get_context(person_id):
+    # retrieve description from datastore
+    pass
