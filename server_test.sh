431f08b0b8c371e709c9695efbfb0301d9918874
alas129 <alas129@163.com>
2024-07-15 18:18:54 -0700

    Uodated the server API testing script.

--- server_test.sh
+++ server_test.sh
@@ -6,8 +6,8 @@ BASE_URL="http://127.0.0.1:8080/db"
 # Test addUser
 echo "Testing addUser..."
 curl -X POST "$BASE_URL/addUser" -H "Content-Type: application/json" -d '{
-  "name": "John Doe",
-  "imageLink": "http://example.com/image.jpg",
+  "name": "Jordan Peterson",
+  "imageLink": "http://example.com/jp_image.jpg",
   "description": "A new user"
 }'
 echo -e "\n"
@@ -15,7 +15,7 @@ echo -e "\n"
 # Test getUser
 echo "Testing getUser..."
 curl -X POST "$BASE_URL/getUser" -H "Content-Type: application/json" -d '{
-  "userId": "1"
+  "userId": 5639601012080640
 }'
 echo -e "\n"
 
@@ -27,8 +27,8 @@ echo -e "\n"
 # Test updateUser
 echo "Testing updateUser..."
 curl -X PUT "$BASE_URL/updateUser" -H "Content-Type: application/json" -d '{
-  "userId": "1",
-  "name": "Jane Doe",
+  "userId": 5642061785071616,
+  "name": "Foo Bar",
   "imageLink": "http://example.com/newimage.jpg",
   "description": "Updated description"
 }'
@@ -37,7 +37,7 @@ echo -e "\n"
 # Test deleteUser
 echo "Testing deleteUser..."
 curl -X DELETE "$BASE_URL/deleteUser" -H "Content-Type: application/json" -d '{
-  "userId": "1"
+  "userId": 5644523313037312
 }'
 echo -e "\n"
 
@@ -46,10 +46,9 @@ echo "Testing addPerson..."
 curl -X POST "$BASE_URL/addPerson" -H "Content-Type: application/json" -d '{
   "name": "Alice",
   "imageLink": "http://example.com/person.jpg",
-  "description": "A new person",
+  "description": "A foo person",
   "context": "Some context",
-  "userId": 1,
-  "collectionId": "collection1",
+  "userId": 5642061785071616,
   "public": true
 }'
 echo -e "\n"
@@ -57,7 +56,11 @@ echo -e "\n"
 # Test getPersonList
 echo "Testing getPersonList..."
 curl -X POST "$BASE_URL/getPersonList" -H "Content-Type: application/json" -d '{
-  "userId": "1"
+  "userId": 5642061785071616,
+  "sortBy": "name",
+  "limit": 10,
+  "page": 1,
+  "order": "asc"
 }'
 echo -e "\n"
 
@@ -82,8 +85,8 @@ echo -e "\n"
 # Test updatePerson
 echo "Testing updatePerson..."
 curl -X PUT "$BASE_URL/updatePerson" -H "Content-Type: application/json" -d '{
-  "personId": "1",
-  "newName": "Bob",
+  "personId": 5723707007827968,
+  "newName": "Foo Bar",
   "newImageLink": "http://example.com/newperson.jpg",
   "newDescription": "Updated person description",
   "newContext": "Updated context",
@@ -94,15 +97,15 @@ echo -e "\n"
 # Test deletePerson
 echo "Testing deletePerson..."
 curl -X DELETE "$BASE_URL/deletePerson" -H "Content-Type: application/json" -d '{
-  "personId": 1
+  "personId": 5665673409724416
 }'
 echo -e "\n"
 
 # Test addCollection
 echo "Testing addCollection..."
 curl -X POST "$BASE_URL/addCollection" -H "Content-Type: application/json" -d '{
-  "userId": "1",
-  "name": "My Collection",
+  "userId": 5642061785071616,
+  "name": "Foo Collection",
   "imageLink": "http://example.com/collection.jpg",
   "description": "A new collection",
   "isPublic": true
@@ -112,7 +115,7 @@ echo -e "\n"
 # Test getCollectionList
 echo "Testing getCollectionList..."
 curl -X POST "$BASE_URL/getCollectionList" -H "Content-Type: application/json" -d '{
-  "userId": "1",
+  "userId": 5642061785071616,
   "page": 1,
   "limit": 10,
   "sortBy": "name",
@@ -130,7 +133,7 @@ echo -e "\n"
 # Test updateCollection
 echo "Testing updateCollection..."
 curl -X PUT "$BASE_URL/updateCollection" -H "Content-Type: application/json" -d '{
-  "collectionId": "collection1",
+  "collectionId": 5680529164730368,
   "newName": "Updated Collection",
   "newImageLink": "http://example.com/newcollection.jpg",
   "newDescription": "Updated collection description",
@@ -141,6 +144,6 @@ echo -e "\n"
 # Test deleteCollection
 echo "Testing deleteCollection..."
 curl -X DELETE "$BASE_URL/deleteCollection" -H "Content-Type: application/json" -d '{
-  "collectionId": "collection1"
+  "collectionId": 5722267187150848
 }'
 echo -e "\n"
