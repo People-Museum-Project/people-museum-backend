ca6b66c9561c3a4a63af45a3e1bea0821cb79f92
alas129 <alas129@163.com>
2024-07-20 16:01:12 -0700

    update the testing script. BASE Url and its routing directory

--- server_test.sh
+++ server_test.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # Base URL
-BASE_URL="http://127.0.0.1:8080/db"
+BASE_URL="http://127.0.0.1:8080"
 
 # Test addUser
 echo "Testing addUser..."
-curl -X POST "$BASE_URL/addUser" -H "Content-Type: application/json" -d '{
+curl -X POST "$BASE_URL/db/addUser" -H "Content-Type: application/json" -d '{
   "name": "Jordan Peterson",
   "imageLink": "http://example.com/jp_image.jpg",
   "description": "A new user"
@@ -14,19 +14,19 @@ echo -e "\n"
 
 # Test getUser
 echo "Testing getUser..."
-curl -X POST "$BASE_URL/getUser" -H "Content-Type: application/json" -d '{
+curl -X POST "$BASE_URL/db/getUser" -H "Content-Type: application/json" -d '{
   "userId": 5639601012080640
 }'
 echo -e "\n"
 
 # Test listUsers
 echo "Testing listUsers..."
-curl -X GET "$BASE_URL/listUsers"
+curl -X GET "$BASE_URL/db/listUsers"
 echo -e "\n"
 
 # Test updateUser
 echo "Testing updateUser..."
-curl -X PUT "$BASE_URL/updateUser" -H "Content-Type: application/json" -d '{
+curl -X PUT "$BASE_URL/db/updateUser" -H "Content-Type: application/json" -d '{
   "userId": 5642061785071616,
   "name": "Foo Bar",
   "imageLink": "http://example.com/newimage.jpg",
@@ -36,14 +36,14 @@ echo -e "\n"
 
 # Test deleteUser
 echo "Testing deleteUser..."
-curl -X DELETE "$BASE_URL/deleteUser" -H "Content-Type: application/json" -d '{
+curl -X DELETE "$BASE_URL/db/deleteUser" -H "Content-Type: application/json" -d '{
   "userId": 5644523313037312
 }'
 echo -e "\n"
 
 # Test addPerson
 echo "Testing addPerson..."
-curl -X POST "$BASE_URL/addPerson" -H "Content-Type: application/json" -d '{
+curl -X POST "$BASE_URL/db/addPerson" -H "Content-Type: application/json" -d '{
   "name": "Alice",
   "imageLink": "http://example.com/person.jpg",
   "description": "A foo person",
@@ -55,7 +55,7 @@ echo -e "\n"
 
 # Test getPersonList
 echo "Testing getPersonList..."
-curl -X POST "$BASE_URL/getPersonList" -H "Content-Type: application/json" -d '{
+curl -X POST "$BASE_URL/db/getPersonList" -H "Content-Type: application/json" -d '{
   "userId": 5642061785071616,
   "sortBy": "name",
   "limit": 10,
@@ -66,7 +66,7 @@ echo -e "\n"
 
 # Test getPersonListByCollection
 echo "Testing getPersonListByCollection..."
-curl -X POST "$BASE_URL/getPersonListByCollection" -H "Content-Type: application/json" -d '{
+curl -X POST "$BASE_URL/db/getPersonListByCollection" -H "Content-Type: application/json" -d '{
   "collectionId": 5675594515742720,
   "sortBy": "name",
   "limit": 10,
@@ -77,14 +77,14 @@ echo -e "\n"
 
 # Test getPerson
 echo "Testing getPerson..."
-curl -X POST "$BASE_URL/getPerson" -H "Content-Type: application/json" -d '{
+curl -X POST "$BASE_URL/db/getPerson" -H "Content-Type: application/json" -d '{
   "personId": "5088723339313152"
 }'
 echo -e "\n"
 
 # Test updatePerson
 echo "Testing updatePerson..."
-curl -X PUT "$BASE_URL/updatePerson" -H "Content-Type: application/json" -d '{
+curl -X PUT "$BASE_URL/db/updatePerson" -H "Content-Type: application/json" -d '{
   "personId": 5723707007827968,
   "newName": "Foo Bar",
   "newImageLink": "http://example.com/newperson.jpg",
@@ -96,14 +96,14 @@ echo -e "\n"
 
 # Test deletePerson
 echo "Testing deletePerson..."
-curl -X DELETE "$BASE_URL/deletePerson" -H "Content-Type: application/json" -d '{
+curl -X DELETE "$BASE_URL/db/deletePerson" -H "Content-Type: application/json" -d '{
   "personId": 5665673409724416
 }'
 echo -e "\n"
 
 # Test addCollection
 echo "Testing addCollection..."
-curl -X POST "$BASE_URL/addCollection" -H "Content-Type: application/json" -d '{
+curl -X POST "$BASE_URL/db/addCollection" -H "Content-Type: application/json" -d '{
   "userId": 5642061785071616,
   "name": "Foo Collection",
   "imageLink": "http://example.com/collection.jpg",
@@ -114,7 +114,7 @@ echo -e "\n"
 
 # Test getCollectionList
 echo "Testing getCollectionList..."
-curl -X POST "$BASE_URL/getCollectionList" -H "Content-Type: application/json" -d '{
+curl -X POST "$BASE_URL/db/getCollectionList" -H "Content-Type: application/json" -d '{
   "userId": 5642061785071616,
   "page": 1,
   "limit": 10,
@@ -125,14 +125,14 @@ echo -e "\n"
 
 # Test getCollection
 echo "Testing getCollection..."
-curl -X POST "$BASE_URL/getCollection" -H "Content-Type: application/json" -d '{
+curl -X POST "$BASE_URL/db/getCollection" -H "Content-Type: application/json" -d '{
   "collectionId": 5675594515742720
 }'
 echo -e "\n"
 
 # Test updateCollection
 echo "Testing updateCollection..."
-curl -X PUT "$BASE_URL/updateCollection" -H "Content-Type: application/json" -d '{
+curl -X PUT "$BASE_URL/db/updateCollection" -H "Content-Type: application/json" -d '{
   "collectionId": 5680529164730368,
   "newName": "Updated Collection",
   "newImageLink": "http://example.com/newcollection.jpg",
@@ -143,14 +143,14 @@ echo -e "\n"
 
 # Test deleteCollection
 echo "Testing deleteCollection..."
-curl -X DELETE "$BASE_URL/deleteCollection" -H "Content-Type: application/json" -d '{
+curl -X DELETE "$BASE_URL/db/deleteCollection" -H "Content-Type: application/json" -d '{
   "collectionId": 5722267187150848
 }'
 echo -e "\n"
 
 # Test getCollectionListByPerson
 echo "Testing getCollectionListByPerson..."
-curl -X POST "$BASE_URL/getCollectionListByPerson" -H "Content-Type: application/json" -d '{
+curl -X POST "$BASE_URL/db/getCollectionListByPerson" -H "Content-Type: application/json" -d '{
   "personId": 5748214695198720,
   "sortBy": "name",
   "ascending": true,
@@ -161,15 +161,23 @@ echo -e "\n"
 
 # Test addPersonCollection
 echo "Testing addPersonCollection..."
-curl -X POST "$BASE_URL/addPersonCollection" -H "Content-Type: application/json" -d '{
+curl -X POST "$BASE_URL/db/addPersonCollection" -H "Content-Type: application/json" -d '{
   "personId": 5748214695198720,
   "collectionId": 5755374237908992
 }'
 echo -e "\n"
 
 # Remove person from collection
-curl -X DELETE "$BASE_URL/deletePersonFromCollection" -H "Content-Type: application/json" -d '{
+curl -X DELETE "$BASE_URL/db/deletePersonFromCollection" -H "Content-Type: application/json" -d '{
     "personId": 5748214695198720,
     "collectionId": 5755374237908992
+}'
+echo -e "\n"
+
+# AI part
+# GenerateText
+curl -X POST "$BASE_URL/ai/generateText" -H "Content-Type: application/json" -d '{
+  "prompt": "What is the capital of France?",
+  "model": "gpt-3.5-turbo"
 }'
 echo -e "\n"
